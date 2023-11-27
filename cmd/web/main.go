1ee779696a65ab433476e54360dac8381cfb8e89
aysf <ansufw@gmail.com>
2023-11-27 05:47:19 +0100

    add session middleware

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,12 +9,15 @@ import (
 	"os"
 	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/aysf/go-stripe/internal/driver"
 	"github.com/aysf/go-stripe/internal/models"
 )
 
 const version = "1.0.0"
 const cssVersion = "1"
+
+var session *scs.SessionManager
 
 type config struct {
 	port int
@@ -37,6 +40,7 @@ type application struct {
 	templateCache map[string]*template.Template
 	version       string
 	DB            models.DBModel
+	Session       *scs.SessionManager
 }
 
 func (app *application) serve() error {
@@ -76,6 +80,10 @@ func main() {
 	}
 	defer conn.Close()
 
+	// set up session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+
 	tc := make(map[string]*template.Template)
 
 	app := &application{
@@ -85,6 +93,7 @@ func main() {
 		templateCache: tc,
 		version:       version,
 		DB:            models.DBModel{DB: conn},
+		Session:       session,
 	}
 
 	err = app.serve()
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,6 +9,7 @@ import (
 func (app *application) routes() http.Handler {
 
 	mux := chi.NewRouter()
+	mux.Use(SessionLoad)
 
 	mux.Get("/", app.Home)
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
