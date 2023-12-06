fa3eacc09784c239f8ab7ba48e3ecdd2ab540e7e
aysf <ansufw@gmail.com>
2023-12-06 06:06:35 +0100

    add subscription page

--- cmd/web/render.go
+++ cmd/web/render.go
@@ -29,8 +29,8 @@ var function = template.FuncMap{
 }
 
 func formatCurrency(n int) string {
-	f := float32(n / 100)
-	// f := float32(n) / float32(100)
+	// f := float32(n / 100)
+	f := float32(n) / float32(100)
 	return fmt.Sprintf("$%.2f", f)
 }
 
