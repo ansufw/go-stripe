8f4640ce16d1164cd08677054e7912315195daf7
aysf <ansufw@gmail.com>
2023-12-10 16:31:50 +0100

    68.add-login-page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -324,3 +324,11 @@ func (app *application) BronzePlanReceipt(w http.ResponseWriter, r *http.Request
 	}
 
 }
+
+// LoginPage displays a login page
+func (app *application) LoginPage(w http.ResponseWriter, r *http.Request) {
+
+	if err := app.renderTemplate(w, r, "login", &templateData{}); err != nil {
+		app.errorLog.Println(err.Error())
+	}
+}
