638f6b0996df9a911d85984514b38c7889877d6a
aysf <ansufw@gmail.com>
2024-01-10 20:18:19 +0100

    140-add-listner-page-nav

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -545,7 +545,7 @@ func (app *application) AllSales(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	allSales, lastPage, totalRecords, err := app.DB.GetAllOrdersPaginated(2, 1)
+	allSales, lastPage, totalRecords, err := app.DB.GetAllOrdersPaginated(payload.PageSize, payload.CurrentPage)
 	if err != nil {
 		app.errorLog.Println("error get all orders", err)
 		app.badRequest(w, r, err)
@@ -560,7 +560,7 @@ func (app *application) AllSales(w http.ResponseWriter, r *http.Request) {
 		Orders       []*models.Order `json:"orders"`
 	}
 
-	resp.CurrentPage = 1
+	resp.CurrentPage = payload.CurrentPage
 	resp.PageSize = payload.PageSize
 	resp.LastPage = lastPage
 	resp.TotalRecords = totalRecords
