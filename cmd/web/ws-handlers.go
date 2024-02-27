062f72199439b387ceeece3191f5e8b0bb24d3aa
Ananto Yusuf Wicaksono <ansufw@gmail.com>
2024-02-27 16:28:12 +0100

    157-ws

---
+++ cmd/web/ws-handlers.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type WebSocketConnection struct {
+	*websocket.Conn
+}
+
+type WsPayload struct {
+	Action      string              `json:"action"`
+	Message     string              `json:"message"`
+	Username    string              `json:"username"`
+	MessageType string              `json:"message_type"`
+	Conn        WebSocketConnection `json:"-"`
+}
+
+type WsJsonResponse struct {
+	Action  string `json:"action"`
+	Message string `json:"message"`
+	UserID  int    `json:"user_id"`
+}
+
+var upgradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+// keep track every client that connected
+var clients = make(map[WebSocketConnection]string)
+
+var wsChan = make(chan WsPayload)
+
+func (app *application) WsEndPoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	app.infoLog.Printf("client connected from %s", r.RemoteAddr)
+	var response WsJsonResponse
+
+	response.Message = "connected to server"
+
+	err = ws.WriteJSON(response)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	conn := WebSocketConnection{Conn: ws}
+	clients[conn] = ""
+
+	go app.ListenForWS(&conn)
+}
+
+func (app *application) ListenForWS(conn *WebSocketConnection) {
+	defer func() {
+		if r := recover(); r != nil {
+			app.errorLog.Printf("ERROR: %v\n", r)
+		}
+	}()
+
+	var payload WsPayload
+
+	for {
+		err := conn.ReadJSON(&payload)
+		if err != nil {
+			// do nothing
+		} else {
+			payload.Conn = *conn
+			wsChan <- payload
+		}
+	}
+}
+
+func (app *application) ListenToWsChannel() {
+	var response WsJsonResponse
+	for {
+		e := <-wsChan
+		switch e.Action {
+		case "deleteUser":
+			response.Action = "logout"
+			response.Message = "Your account has been deleted"
+			app.broadcastToAll(response)
+
+		default:
+		}
+	}
+}
+
+func (app *application) broadcastToAll(response WsJsonResponse) {
+	for client := range clients {
+		// broadcast to every connected client
+		err := client.WriteJSON(response)
+		if err != nil {
+			app.errorLog.Printf("Websocket error on %s: %s", response.Action, err)
+			_ = client.Close()
+			delete(clients, client)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.20
 
 require golang.org/x/crypto v0.16.0
 
+require golang.org/x/net v0.17.0 // indirect
+
 require (
 	github.com/alexedwards/scs/mysqlstore v0.0.0-20231113091146-cef4b05350c8 // indirect
 	github.com/alexedwards/scs/v2 v2.7.0 // indirect
@@ -11,6 +13,7 @@ require (
 	github.com/go-chi/chi/v5 v5.0.10 // indirect
 	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/gorilla/websocket v1.5.1
 	github.com/stripe/stripe-go/v76 v76.3.0 // indirect
 	github.com/toorop/go-dkim v0.0.0-20201103131630-e1cd1a0a5208 // indirect
 	github.com/xhit/go-simple-mail/v2 v2.16.0 // indirect
